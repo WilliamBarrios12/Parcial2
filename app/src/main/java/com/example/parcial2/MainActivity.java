fe0bfe82de71e8864fa397cf25bcb9eeb2b33852
WilliamBarrios12 <willybarriosrivera@gmail.com>
2024-05-03 16:20:40 -0500

    Cambios en la validacion

--- app/src/main/java/com/example/parcial2/MainActivity.java
+++ app/src/main/java/com/example/parcial2/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn_is;
     EditText ed_usuario;
+
+    EditText ed_contraseña;
     SharedPreferences sharedPreferences;
 
     @Override
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_is = findViewById(R.id.btn_is);
         ed_usuario = findViewById(R.id.ed_usuario);
+        ed_contraseña = findViewById(R.id.ed_contraseña);
 
         sharedPreferences = getSharedPreferences("dataUser", MODE_PRIVATE);
 
@@ -39,16 +43,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String username = ed_usuario.getText().toString();
-                if (!username.isEmpty()) {
+                String password = ed_contraseña.getText().toString();
+
+                if (!username.isEmpty()&&!password.isEmpty()) {
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putString("user", username);
                     editor.apply();
+
+                    Log.d("MainActivity", "Usuario guardado correctamente: " + username);
 
                     Intent i = new Intent(MainActivity.this, MainAdaptador.class);
                     startActivity(i);
                     finish();
                 } else {
-                    Toast.makeText(MainActivity.this, "Por favor ingresa un usuario", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Por favor completa todos los campos", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
